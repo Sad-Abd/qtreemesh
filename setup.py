48cde961d658c269dfe9da66060a56e9ea9ebb19
Sad-Abd <abedisadjad@gmail.com>
2023-07-06 19:59:05 +0330

    modifying bug tracker url in setup.py file

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type = "text/markdown",
     url = "https://github.com/Sad-Abd/qtreemesh",
     project_urls = {
-        "Bug Tracker": "package issues URL",
+        "Bug Tracker": "https://github.com/Sad-Abd/qtreemesh",
     },
     classifiers = [
         "Programming Language :: Python :: 3",
